49b41f31e400738d751af2bca6140d9a276b6e24
cld338 <jihuno291@gmail.com>
2023-08-18 14:41:52 +0900

    modified:   main.py

--- main.py
+++ main.py
@@ -7,32 +7,40 @@ import numpy as np
 import time
 
 def process_receive():
+    n_trials = 0
     while True:
         signal = lsl.receiveData()
-        print("received")
+        # print("received")
         signal_ls.append(signal)
+        n_trials += 1
+        print(len(signal_ls))
 
 
 def process_classification():
     while True:
         print("process_classification running")
-        time.sleep(0.0001)
+        time.sleep(1)
 
 
 lsl = LSL()
 lsl.connect()
-signal_ls = []
+processManager = mp.Manager()
+signal_ls = processManager.list()
+
+windowWidth = 256
+windowOverlap = round(windowWidth*0.5)
 
 if __name__ == "__main__":
-    p1 = mp.Process(target=process_receive, name="receiver", daemon=True)
+    mp.freeze_support()
+    p1 = mp.Process(target=process_receive, name="receiver")
     # print("Status: ", p.is_alive())
-    p2 = mp.Process(target=process_classification, name="classifier", daemon=True)
-
+    p2 = mp.Process(target=process_classification, name="classifier")
+    
     p1.start()
     p2.start()
 
-    while True:
-        print(len(signal_ls))
+    p1.join()
+    p2.join()
 
 
     # time.sleep(5)   # 메인 프로세스 3초 대기
